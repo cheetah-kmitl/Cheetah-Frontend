fc62562bfbd9102ef2a91d8656a6449e938c0897
applentk <applentk@users.noreply.github.com>
2024-10-02 16:52:14 +0700

    Add router and config route

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import './index.css'
+
+import { RootLayout } from './layout/RootLayout';
+import { PrivateLayout } from './layout/PrivateLayout';
+
+import { HomePage } from './pages/HomePage';
+import { AuthPage } from './pages/AuthPage';
+import { DashboardPage } from './pages/DashboardPage';
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/",     element: <HomePage /> },
+      { path: "/auth", element: <AuthPage /> },
+      {
+        element: <PrivateLayout />,
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> }
+        ]
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
-)
+);
