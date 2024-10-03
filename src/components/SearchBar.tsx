88574f138c281a502c8f32c8fd3ea436b093860c
applentk <tanoobguy12@gmail.com>
2024-10-03 17:03:36 +0700

    Adjust height and font size

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,14 +7,14 @@ interface SearchBarProps {
 
 export const SearchBar = ({ onType }: SearchBarProps) => {
   return (
-    <div className="flex items-center relative h-12">
+    <div className="flex items-center relative h-10">
       <SearchIcon 
         className="absolute ml-2 size-6 text-zinc-500"
       />
       <input
         onChange={(e) => onType(e.target.value)}
         placeholder="Search file or folder"
-        className="bg-zinc-200 h-full w-96 text-xl text-center rounded-xl"
+        className="bg-zinc-200 h-full w-96 text-lg text-center rounded-xl active:border pl-10 pr-4"
       />
     </div>
   );
