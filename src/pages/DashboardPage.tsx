72115696c0959a5f5210789af17224ef56837eb6
applentk <tanoobguy12@gmail.com>
2024-10-03 17:01:35 +0700

    Add components and try fetch files

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,47 +3,103 @@ import { SearchBar } from "../components/SearchBar";
 import { LogoHomeNavigate } from "../components/LogoHomeNavigate";
 import { DiskProgressBar } from "../components/DiskProgressBar";
 import { Dropdown } from "../components/Dropdown";
+
 import { SignOutButton } from "@clerk/clerk-react";
+import { AddOptions } from "../components/AddOptions";
+import { ActionOption, File } from "../type";
+import { useFetch } from "../hooks/useFetch";
+import { FileUI } from "../components/FileUI";
+
+const sortByOption  = ["Name", "Date"];
+const orderByOption = ["Name", "Type"];
 
 export const DashboardPage = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
-  
+  const [files, setFiles] = useState<File[]>([]);
+
+  const filesFetcher = useFetch<File[]>("/data.json", "GET");
+
   useEffect(() => {
-    console.log(searchQuery);
-  }, [searchQuery]);
+    const fetchFiles = async () => {
+      const data = await filesFetcher.fetchData();
+      if (data !== null) {
+        setFiles(data);
+      }
+    }
+
+    fetchFiles();
+  }, []);
+
+  const addButtonOptions: ActionOption[] = [
+    {
+      name: "Upload file",
+      action: () => {}
+    },
+    {
+      name: "Upload something",
+      action: () => {}
+    }
+  ];
 
   return (
     <>
-      <header className="w-full px-10 py-6 flex justify-between items-start">
-        <div className="h-full">
+      <header className="w-full px-10 py-6 grid grid-cols-3">
+        <div className="h-full w-48 -mt-2">
           <LogoHomeNavigate />
           <DiskProgressBar
+            used={1}
             totalGB={10}
-            used={5}
           />
         </div>
         <SearchBar
           onType={(query) => setSearchQuery(query)}
         />
-        <SignOutButton
-          redirectUrl="/auth"
-        />
+        <div className="justify-self-end">
+          <SignOutButton
+            redirectUrl="/auth"
+          />
+        </div>
       </header>
 
-      <main className="flex items-center w-4/5">
-        <div className="flex bg-slate-300">
+      <main className="flex flex-col items-center w-full">
+        <div className="flex justify-end gap-4 w-4/5 border-b-2 pb-2">
           <Dropdown
             title="Sort by"
-            options={[]}
+            options={sortByOption}
             onSelect={(option) => console.log(option)}
           />
           <Dropdown
             title="Order by"
-            options={[]}
+            options={orderByOption}
             onSelect={(option) => console.log(option)}
           />
         </div>
+
+        <div className="mt-4 w-[80%] h-auto">
+          { files.length === 0 ?
+            <i>
+              Looks like you don't have a file, Add some?
+            </i>
+            :
+            <ul className="grid grid-cols-4 w-full gap-12 p-4">
+              { files
+                .filter((file) =>
+                  file.name.toLowerCase().includes(searchQuery.toLowerCase())
+                )
+                .map((file, index) =>
+                  <li key={index}>
+                    <FileUI file={file} />
+                  </li>
+                )
+              }
+            </ul> 
+          }
+        </div>
       </main>
+
+      <AddOptions
+        options={addButtonOptions}
+      />
     </>
   );
 }
