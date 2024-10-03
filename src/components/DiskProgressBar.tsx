1fbed8ff4b7ced0030ff8fd751aba41267d0bb24
applentk <tanoobguy12@gmail.com>
2024-10-03 17:02:52 +0700

    Remove log and adjust font size

--- src/components/DiskProgressBar.tsx
+++ src/components/DiskProgressBar.tsx
@@ -10,11 +10,9 @@ export const DiskProgressBar = ({ totalGB, used }: DiskProgressBarProps) => {
   if (used > totalGB)
     used = totalGB;
 
-  console.log((used / 100) * totalGB);
-
   return (
     <div>
-      <div className="flex justify-between text-lg">
+      <div className="flex justify-between">
         <h1 className="font-bold">
           { used } GB
         </h1>
@@ -24,7 +22,7 @@ export const DiskProgressBar = ({ totalGB, used }: DiskProgressBarProps) => {
       </div>
 
       {/* Progress bar */}
-      <div className="h-4 p-1 bg-zinc-300 rounded-full">
+      <div className="h-3 p-0.5 bg-zinc-300 rounded-full">
         <div
           style={{ width: `${ used / totalGB * 100 }%` }}
           className="h-full bg-zinc-500 rounded-full"
