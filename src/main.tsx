b44d6aba64e9d3b23cfd9560ff2808bd7079012d
applentk <applentk@users.noreply.github.com>
2024-10-04 23:34:00 +0700

    Remove router

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import './index.css'
 
-import { RootLayout } from './layout/RootLayout';
-import { PrivateLayout } from './layout/PrivateLayout';
-
-import { HomePage } from './pages/HomePage';
-import { AuthPage } from './pages/AuthPage';
 import { DashboardPage } from './pages/DashboardPage';
-
-const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      { path: "/",     element: <HomePage /> },
-      { path: "/auth", element: <AuthPage /> },
-      {
-        element: <PrivateLayout />,
-        children: [
-          { path: "/dashboard", element: <DashboardPage /> }
-        ]
-      }
-    ]
-  }
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <DashboardPage />
   </StrictMode>
 );
