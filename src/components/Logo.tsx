d05651ecd9eb765d1cb54e48b09dedb8f177a1ed
applentk <applentk@users.noreply.github.com>
2024-10-16 21:17:15 +0700

    Change style

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,11 @@
 import CheetahLogo from "/cheetah-logo.png";
 
-export const Logo = () => {
+export const Logo = ({ className }: React.HTMLProps<HTMLDivElement>) => {
   return (
     <img
       src={CheetahLogo}
       alt="logo"
-      className="cursor-pointer -ml-1"
+      className={`-ml-1 ${className}`}
     />
   );
 }
