ae280f633468c3732a3691d1774a9994bbb8c0f8
applentk <applentk@users.noreply.github.com>
2024-10-16 21:18:21 +0700

    Add files container, upload files and other stuff

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,103 +1,182 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
+
+import { File } from "../ts/type";
+
 import { SearchBar } from "../components/SearchBar";
 import { Logo } from "../components/Logo";
-import { DiskProgressBar } from "../components/DiskProgressBar";
 import { Dropdown } from "../components/Dropdown";
+import { FileDropZone } from "../components/FileDropZone";
+import { FilesContainer } from "../components/FilesContainer";
+import { addCommas } from "../ts/utils";
+import { Button } from "../components/Button";
+import { LoadingAnimation } from "../components/LoadingAnimation";
 
-import { AddOptions } from "../components/AddOptions";
-import { ActionOption, File } from "../type";
-import { useFetch } from "../hooks/useFetch";
-import { FileUI } from "../components/FileUI";
+const pages = ["Files", "Upload"];
+const sortByOption = ["Name", "Size", "Upload Date"]
 
-const sortByOption  = ["Name", "Date"];
-const orderByOption = ["Name", "Type"];
+const apiServerURL = import.meta.env.VITE_API_SERVER_URL;
+
+const dataLimit = 30;
+
+let isHaveDataLeft = true;
+let dataPageIndex = 0;
 
 export const DashboardPage = () => {
-  const [searchQuery, setSearchQuery] = useState<string>("");
   const [files, setFiles] = useState<File[]>([]);
+  const [filesSize, setFilesSize] = useState<number>(0);
+  const [sortOption, setSortOption] = useState<string>("upload_at");
+  const [currentPage, setCurrentPage] = useState<string>("Files");
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
-  const filesFetcher = useFetch<File[]>("/data.json", "GET");
+  const [isFetching, setIsFetching] = useState<boolean>(true);
+  const [isFetchError, setIsFetchError] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchFiles = async () => {
-      const data = await filesFetcher.fetchData();
-      if (data !== null) {
-        setFiles(data);
+  console.log(isFetching);
+
+  async function fetchFiles() {
+    try {
+      setIsFetching(true);
+      const response = await axios.get(
+        `${apiServerURL}/api/file?limit=${dataLimit}`,
+        { withCredentials: true }
+      );
+      const files: File[] = response.data.data;
+
+      dataPageIndex++;
+      setFiles(files);
+    }
+    catch {
+      setIsFetchError(true);
+    }
+    finally {
+      setIsFetching(false);
+    }
+  };
+
+  async function scrollBottomHandle(e: React.UIEvent<HTMLElement>) {
+    if (!e.currentTarget) return;
+    const page = e.currentTarget;
+    const isScrollBottom = (page.scrollHeight - page.scrollTop == page.clientHeight);
+
+    if (isScrollBottom && isHaveDataLeft) {
+      try {
+        setIsFetching(true);
+
+        const dataStart = dataLimit * dataPageIndex;
+        const response = await axios.get(
+          `${apiServerURL}/api/file?start=${dataStart}limit=${dataLimit}&sortingBy=upload_at&`,
+          { withCredentials: true }
+        );
+
+        const files: File[] = response.data.data;
+
+        if (files.length === 0) {
+          isHaveDataLeft = false;
+          return;
+        }
+
+        dataPageIndex++;
+        setFiles((prevFiles) => [ ...prevFiles, ...files ]);
+      }
+      catch {
+        setIsFetchError(true);
+      }
+      finally {
+        setIsFetching(false);
       }
     }
+  };
+
+  useEffect(() => {
     fetchFiles();
+  }, []);
 
-  }, [filesFetcher]);
+  useEffect(() => {
+    const totalSize = files
+    .map((file) => file.info.size)
+    .reduce((sum, current) => sum += current, 0); 
 
-  const addButtonOptions: ActionOption[] = [
-    {
-      name: "Upload file",
-      action: () => {}
-    },
-    {
-      name: "Upload something",
-      action: () => {}
+    setFilesSize(totalSize);
+
+  }, [files]);
+
+  function getPage(page: string): JSX.Element {
+    switch (page) {
+      case "Files" :
+        return (
+          <FilesContainer
+            sortOption={sortOption}
+            query={searchQuery}
+            files={files}
+          />
+        );
+
+      case "Upload":
+        return <FileDropZone />
     }
-  ];
+
+    return <></>;
+  };
 
   return (
-    <>
-      <header className="w-full px-10 py-6 grid grid-cols-3">
-        <div className="h-full w-48 -mt-2">
-          <Logo />
-          <DiskProgressBar
-            used={1}
-            totalGB={10}
-          />
-        </div>
-        <SearchBar
-          defaultValue={searchQuery}
-          className="justify-self-center"
-          onType={(query) => {
-            setSearchQuery(query);
-          }}
+    <section
+      onScroll={scrollBottomHandle}
+      className="h-screen overflow-y-auto"
+    >
+      <header className="w-full px-6 py-6 grid grid-cols-3">
+        <Logo
+          className="h-full w-48 -mt-2"
         />
+        { currentPage === "Files" ?
+          <SearchBar
+            defaultValue={searchQuery}
+            className="justify-self-center"
+            onType={(query) => setSearchQuery(query)}
+          />
+          :
+          <div/>
+        }
+        <div className="min-w-44 text-lg justify-self-end text-end font-semibold text-zinc-700">
+          Total Used: <span className="font-bold text-lg text-black">{ addCommas(filesSize) }</span> KB
+        </div>
       </header>
 
       <main className="flex flex-col items-center w-full">
-        <div className="flex justify-end gap-4 w-3/4 border-b-2 pb-2">
-          <Dropdown
-            title="Sort by"
-            options={sortByOption}
-            onSelect={(option) => console.log(option)}
-          />
-          <Dropdown
-            title="Order by"
-            options={orderByOption}
-            onSelect={(option) => console.log(option)}
-          />
+        <div className="flex justify-between w-3/4 border-b-2 pb-2">
+          <div className="flex gap-6 text-lg">
+            { pages.map((page, index) => 
+                <Button
+                  key={index}
+                  onClick={() => setCurrentPage(page)}
+                  selecting={currentPage === page}
+                >
+                  { page }
+                </Button>
+            )}
+          </div>
+          { currentPage === "Files" && 
+            <Dropdown
+              title="Sort by"
+              options={sortByOption}
+              onSelect={(option) => setSortOption(option)}
+            />
+          }
         </div>
 
-        <div className="mt-4 w-3/4 h-auto">
-          { files.length === 0 ?
-            <i>
-              Looks like you don't have a file, Add some?
+        <div className="flex flex-col items-center gap-2 mt-6 mb-12 w-3/4 h-auto text-center">
+          { isFetching && 
+            <LoadingAnimation />
+          }
+          { isFetchError ?
+            <i className="text-red-600">
+              Failed to reach server please wait and reload the page to try again.
             </i>
             :
-            <ul className="grid grid-cols-5 w-full gap-12 p-4">
-              { files
-                .filter((file) =>
-                  file.name.toLowerCase().includes(searchQuery.toLowerCase())
-                )
-                .map((file, index) =>
-                  <li key={index}>
-                    <FileUI file={file} />
-                  </li>
-                )
-              }
-            </ul> 
+            getPage(currentPage)
           }
         </div>
       </main>
-
-      <AddOptions
-        options={addButtonOptions}
-      />
-    </>
+    </section>
   );
 }
